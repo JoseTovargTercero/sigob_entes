6c7e48d62912904cd98c16402f1ef30669ec4376
José Ricardo Tovarg III <jose.2710.ricardo@gmail.com>
2024-12-29 11:30:47 -0400

    `Update config.php to use sigob.net instead of gitcom-ve.com`

--- back/sistema_global/config.php
+++ back/sistema_global/config.php
@@ -6,7 +6,7 @@ if ($_SERVER['SERVER_NAME'] == 'localhost') {
     define('URL', 'http://localhost/sigob_entes/');
     define('PASSWORD', "");
     define('USER', 'root');
-} elseif ($_SERVER['SERVER_NAME'] == 'gitcom-ve.com') {
+} elseif ($_SERVER['SERVER_NAME'] == 'sigob.net') {
     define('URL', 'https://sigob.net/');
     define('PASSWORD', "]n^VmqjqCD1k");
     define('USER', 'sigobnet_userroot');
