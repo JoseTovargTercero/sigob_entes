3c2a2f9cc97652e55d699286e5fa46c98e4e251b
JessZB <jesusnbz22@gmail.com>
2025-01-05 19:41:58 -0400

    Pruebas para realizar peticiones desde local host

--- api/index.php
+++ api/index.php
@@ -2,9 +2,17 @@
 
 require_once 'controllers/routes.controller.php';
 
-// CORS
-// Permitir acceso desde cualquier origen
-header('Access-Control-Allow-Origin: *');
+$allowedOrigins = [
+    'http://localhost',      // Correcto para localhost sin puerto
+    'http://localhost:3000', // Correcto para localhost con puerto 3000
+    // Añade aquí los orígenes de producción cuando despliegues
+];
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+} else {
+    header('Access-Control-Allow-Origin: *');
+}
 // Permitir métodos específicos
 header('Access-Control-Allow-Methods: GET, POST, PUT');
 
