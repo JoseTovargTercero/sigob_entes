e14753e005f487a590215098f308c82697cc2d6c
JessZB <jesusnbz22@gmail.com>
2025-01-05 19:42:24 -0400

    Refactorizar configuración de CORS en el controlador de rutas

--- api/index.php
+++ api/index.php
@@ -12,6 +12,7 @@ if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed
     header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
 } else {
     header('Access-Control-Allow-Origin: *');
+
 }
 // Permitir métodos específicos
 header('Access-Control-Allow-Methods: GET, POST, PUT');
