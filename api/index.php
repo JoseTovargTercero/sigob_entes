05a0943f4140702731d37869507d809756d63d57
JessZB <jesusnbz22@gmail.com>
2025-01-05 19:31:03 -0400

    Añadir headers de origen en api

--- api/index.php
+++ api/index.php
@@ -2,6 +2,11 @@
 
 require_once 'controllers/routes.controller.php';
 
+// CORS
+// Permitir acceso desde cualquier origen
+header('Access-Control-Allow-Origin: *');
+// Permitir métodos específicos
+header('Access-Control-Allow-Methods: GET, POST, PUT');
 
 $index = new RoutesController();
 
