c50802f88350a7df96cf335b15ca04e4960498ac
Jesús Zapata <jesusnbz22@gmail.com>
2025-01-06 11:23:56 -0400

    Refactorizar configuración de CORS y mejorar reglas de reescritura en .htaccess

--- api/index.php
+++ api/index.php
@@ -1,11 +1,10 @@
 <?php
 
-//  Permitir solicitudes desde http://localhost 
-header("Access-Control-Allow-Origin: *");
-// Permitir métodos HTTP específicos 
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-// Permitir encabezados personalizados 
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Origin: http://localhost"); // Reemplaza con el/los origen/es permitidos
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
+header('Access-Control-Allow-Credentials: true'); // Opcional si se envían credenciales
+
 
 require_once 'controllers/routes.controller.php';
 
