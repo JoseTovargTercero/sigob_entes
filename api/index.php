64a8d504b170ef139edad0755e379179b96e4663
JessZB <jesusnbz22@gmail.com>
2025-01-05 21:17:15 -0400

    Eliminar cabeceras innecesarios

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
 //  Permitir solicitudes desde http://localhost 
-header("Access-Control-Allow-Origin: http://localhost");
+header("Access-Control-Allow-Origin: *");
 // Permitir métodos HTTP específicos 
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 // Permitir encabezados personalizados 
@@ -9,11 +9,6 @@ header("Access-Control-Allow-Headers: Content-Type");
 
 require_once 'controllers/routes.controller.php';
 
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-// Permitir métodos específicos
-header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
 
 $index = new RoutesController();
 
