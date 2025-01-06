6b4ceb7834f9f53b08eea798090d697dfbe14d4b
JessZB <jesusnbz22@gmail.com>
2025-01-05 20:33:55 -0400

    headers test

--- api/index.php
+++ api/index.php
@@ -3,17 +3,14 @@
 require_once 'controllers/routes.controller.php';
 
 $allowedOrigins = [
-    'http://localhost',      // Correcto para localhost sin puerto
+    '',      // Correcto para localhost sin puerto
     'http://localhost:3000', // Correcto para localhost con puerto 3000
     // Añade aquí los orígenes de producción cuando despliegues
 ];
 
-if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
-    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-} else {
-    header('Access-Control-Allow-Origin: *');
 
-}
+header('Access-Control-Allow-Origin: http://localhost');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 // Permitir métodos específicos
 header('Access-Control-Allow-Methods: GET, POST, PUT');
 
