e3ed7b5f47923e3eac0a09fb26cb24d4bc4052b1
JessZB <jesusnbz22@gmail.com>
2025-01-05 20:39:46 -0400

    Cambio en headers, test

--- api/index.php
+++ api/index.php
@@ -2,14 +2,8 @@
 
 require_once 'controllers/routes.controller.php';
 
-$allowedOrigins = [
-    '',      // Correcto para localhost sin puerto
-    'http://localhost:3000', // Correcto para localhost con puerto 3000
-    // Añade aquí los orígenes de producción cuando despliegues
-];
 
-
-header('Access-Control-Allow-Origin: http://localhost');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 // Permitir métodos específicos
 header('Access-Control-Allow-Methods: GET, POST, PUT');
