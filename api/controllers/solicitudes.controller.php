6abca8184e0dae4e869e4e3ce67569ae4a4d4ab0
Jesús Zapata <jesusnbz22@gmail.com>
2025-01-09 17:37:03 -0400

    Agregar confirmación de transacción al aceptar una solicitud

--- api/controllers/solicitudes.controller.php
+++ api/controllers/solicitudes.controller.php
@@ -562,6 +562,7 @@ class SolicitudesController
                 $stmtUpdateSolicitud->execute();
 
                 if ($stmtUpdateSolicitud->affected_rows > 0) {
+                    $this->conexion->commit();
                     return ["success" => "Solicitud Aceptada con éxito."];
                 } else {
                     throw new Exception("No se pudo actualizar la solicitud a aceptada");
