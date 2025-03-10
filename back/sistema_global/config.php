719bfcd43260addb1a0182e5bb65627c2e5e7d90
Moises Celis <143418330+moisescelis21@users.noreply.github.com>
2025-03-10 12:10:48 -0400

    Update config.php

--- back/sistema_global/config.php
+++ back/sistema_global/config.php
@@ -3,25 +3,27 @@
 
 
 if ($_SERVER['SERVER_NAME'] == 'localhost') {
+    define('HOST', 'localhost');
+define('DB', 'sigobnet_sigob_entes');
+define('CHARSET', 'utf8mb4');
     define('URL', 'http://localhost/sigob_entes/');
     define('PASSWORD', "");
     define('USER', 'root');
 } elseif ($_SERVER['SERVER_NAME'] == 'sigob.net') {
+    define('HOST', 'sigob.net');
+    define('DB', 'sigobnet_sigob_entes');
+    define('CHARSET', 'utf8mb4');
     define('URL', 'https://sigob.net/');
     define('PASSWORD', "]n^VmqjqCD1k");
     define('USER', 'sigobnet_userroot');
 } else {
-    $url = 'http://' . $_SERVER['SERVER_NAME'] . '/sigob/';
+    $url = 'http://' . $_SERVER['SERVER_NAME'] . '/sigob_entes/';
+    define('HOST', 'sigob.net');
+    define('DB', 'sigobnet_sigob_entes');
+    define('CHARSET', 'utf8mb4');
     define('URL', $url);
-    define('PASSWORD', "");
-    define('USER', 'root');
+    define('PASSWORD', "]n^VmqjqCD1k");
+    define('USER', 'sigobnet_userroot');
 }
 
 
-
-
-
-
-define('HOST', 'localhost');
-define('DB', 'sigobnet_sigob_entes');
-define('CHARSET', 'utf8mb4');
